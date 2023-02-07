a3ea5936aff633f59543053a7de65272035887b5
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2023-02-07 09:46:06 -0300

    adicionando o link do back-end railway ao .env

--- front-end/src/service/request.js
+++ front-end/src/service/request.js
@@ -24,7 +24,7 @@ const api = {
         '/login',
         userData,
       );
-      console.log(env.API);
+      console.log(process.env.API);
       return response;
     },
     async register(userData) {
