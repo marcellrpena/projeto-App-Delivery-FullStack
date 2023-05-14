2bbb10b8feb0dfcf32c5e2c55ebd4fcf93674a88
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2023-05-14 17:51:47 -0300

    adicionando validação de token aos endpoints do back-end

--- back-end/src/routes/index.router.js
+++ back-end/src/routes/index.router.js
@@ -4,15 +4,16 @@ const productRouter = require('./product.router');
 const { saleRouter } = require('./sale.router');
 const userRouter = require('./user.router');
 const ordersRouter = require('./orders.router');
+const { tokenValidation } = require('../middlewares/tokenValidation');
 
 const routers = express.Router();
 
 routers.use('/login', loginRouter);
-routers.use('/register', userRouter);
-routers.use('/users', userRouter);
-routers.use('/products', productRouter);
+routers.use('/register', tokenValidation, userRouter);
+routers.use('/users', tokenValidation, userRouter);
+routers.use('/products', tokenValidation, productRouter);
 routers.use('/sales', saleRouter);
 
-routers.use('/seller/orders', ordersRouter);
+routers.use('/seller/orders', tokenValidation, ordersRouter);
 
 module.exports = routers;
