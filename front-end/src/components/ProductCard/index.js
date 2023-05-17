a2491e6b2045063f38f16e3e970a86356d3ba995
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2023-05-17 13:26:26 -0300

    manipulaçao de dotenv

--- front-end/src/components/ProductCard/index.js
+++ front-end/src/components/ProductCard/index.js
@@ -1,6 +1,8 @@
 import PropTypes, { string, number } from 'prop-types';
 import { useState, useEffect } from 'react';
 
+require('dotenv').config();
+
 const HOST = process.env.REACT_APP_API_HOST || 'localhost';
 const PROTOCOL = process.env.REACT_APP_API_PROTOCOL || 'https';
 
